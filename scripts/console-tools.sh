aba822ae042facf1ae9f061946f62add6a034b2e
Timoteo Ramos <timoteoramos@ymail.com>
2019-01-26 01:17:37 -0300

    Console tools

---
+++ scripts/console-tools.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xbps-install -S curl htop ranger tmux vim-huge-python3 wget zsh
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ SCRIPT_ROOT=$(cd `dirname "$0"` && pwd)/scripts
 
 xbps-install -Su
 bash $SCRIPT_ROOT/daemons.sh
+bash $SCRIPT_ROOT/console-tools.sh
