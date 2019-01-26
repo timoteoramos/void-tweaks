7615ea26c647c6903eb0a5f5c692e66593eb7ed2
Timoteo Ramos <timoteoramos@ymail.com>
2019-01-26 01:20:29 -0300

    Development tools

---
+++ scripts/dev-tools.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xbps-install -S base-devel bzr cmake cvs git mercurial python3-{Cython,devel,pip} subversion virtualenvwrapper
--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ SCRIPT_ROOT=$(cd `dirname "$0"` && pwd)/scripts
 xbps-install -Su
 bash $SCRIPT_ROOT/daemons.sh
 bash $SCRIPT_ROOT/console-tools.sh
+bash $SCRIPT_ROOT/dev-tools.sh
