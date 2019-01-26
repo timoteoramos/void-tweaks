e4bbdaafff250b0b02d17a16da6573fdfe90f41f
Timoteo Ramos <timoteoramos@ymail.com>
2019-01-26 02:07:20 -0300

    Bluetooth and CUPS daemons

--- scripts/daemons.sh
+++ scripts/daemons.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-xbps-install -S chrony dbus NetworkManager{,-{open{connect,vpn},vpnc}}
+xbps-install -S bluez{,-{alsa,cups}} chrony cups{,-{filters,pk-helper}} dbus NetworkManager{,-{open{connect,vpn},vpnc}}
 
+[ ! -L /var/service/bluetoothd ] && ln -s /etc/sv/bluetoothd /var/service/
 [ ! -L /var/service/chronyd ] && ln -s /etc/sv/chronyd /var/service/
+[ ! -L /var/service/cupsd ] && ln -s /etc/sv/cupsd /var/service/
 [ ! -L /var/service/dbus ] && ln -s /etc/sv/dbus /var/service/
 [ -L /var/service/wpa_supplicant ] && rm /var/service/wpa_supplicant
 [ -L /var/service/dhcpcd ] && rm /var/service/dhcpcd
